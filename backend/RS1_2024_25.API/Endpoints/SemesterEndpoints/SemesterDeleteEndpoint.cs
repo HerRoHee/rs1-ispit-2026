b2d6528be722531cf0d40c2dc69b62774bc22d87
Esmir <esmir.hero@outlook.com>
2026-02-06 11:28:34 +0100

    Semester Delete & Restore Endpoints added
    Endpointi za delete i restore semestra implementirani
    Podešavanje IsDeleted polja na true/false u ovisnosti od endpointa

---
+++ backend/RS1_2024_25.API/Endpoints/SemesterEndpoints/SemesterDeleteEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RS1_2024_25.API.Data;
+using RS1_2024_25.API.Helper.Api;
+
+namespace RS1_2024_25.API.Endpoints.SemesterEndpoints
+{
+    [Route("student-semesters")]
+    public class SemesterDeleteEndpoint(ApplicationDbContext db) : MyEndpointBaseAsync
+        .WithRequest<int>
+        .WithActionResult
+    {
+        [HttpDelete("{id}")]
+        public override async Task<ActionResult> HandleAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var semester = await db.SemestersAll.FindAsync(id, cancellationToken);
+
+            if (semester == null)
+                return NotFound("Semester not found");
+
+            if (semester.IsDeleted)
+                return BadRequest("Semester already deleted");
+
+            semester.IsDeleted = true;
+            await db.SaveChangesAsync(cancellationToken);
+
+            return Ok("Semester deleted successfully");
+        }
+    }
+}
---
+++ backend/RS1_2024_25.API/Endpoints/SemesterEndpoints/SemesterRestoreEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RS1_2024_25.API.Data;
+using RS1_2024_25.API.Helper.Api;
+
+namespace RS1_2024_25.API.Endpoints.SemesterEndpoints
+{
+    [Route("student-semesters")]
+    public class SemesterRestoreEndpoint(ApplicationDbContext db) : MyEndpointBaseAsync
+        .WithRequest<int>
+        .WithActionResult
+    {
+        [HttpPost("{id}/restore")]
+        public override async Task<ActionResult> HandleAsync(int id, CancellationToken cancellationToken = default)
+        {
+            var semester = await db.SemestersAll.FindAsync(id, cancellationToken);
+
+            if (semester == null)
+                return NotFound("Semester not found");
+
+            if (!semester.IsDeleted)
+                return BadRequest("Semester is not deleted");
+
+            semester.IsDeleted = false;
+            await db.SaveChangesAsync(cancellationToken);
+
+            return Ok("Semester restored successfully");
+        }
+    }
+}
